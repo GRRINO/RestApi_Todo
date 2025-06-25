4d9577d0a35737159a77ed30e4cb59067626cc5e
GRRINO <yy79250@gmail.com>
2025-06-26 00:26:27 +0630

    Deploy4

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 // import bcrypt from "bcrypt";
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 interface IUser extends Document {
   name: string;
